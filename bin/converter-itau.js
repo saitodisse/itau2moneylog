aec437011199dcba14dc3df74e5d8effefd29c3c
Julio Makdisse Saito <saitodisse@gmail.com>
2015-06-16 12:24:04 -0300

    Cli

---
+++ bin/converter-itau.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+require('source-map-support').install();
+
+var path = require('path');
+var fs = require('fs');
+
+var docopt = require('docopt').docopt;
+
+var doc = fs.readFileSync(path.join(__dirname, 'usage.txt')).toString();
+var result = docopt(doc, {
+    argv: process.argv.slice(2),
+    help: true,
+    version: '0.1.0'
+  });
+
+var ConvertItauCli = require('../lib/src/cli/convert-itau-cli');
+var cli = new ConvertItauCli({
+  paste_data_path: result['<paste_path>']
+});
+
+cli.convert();
---
+++ src/cli/convert-itau-cli.js
@@ -0,0 +1,25 @@
+// import ConvertFileSimples from '../extrato-simples/convert-file';
+import ConvertFilePeriodo from '../periodo-especifico/convert-file';
+
+/// origin_file_path
+module.exports = class ConvertItauCli {
+  constructor(opts = {}) {
+    Object.keys(opts).forEach((key) => {
+      if (opts.hasOwnProperty(key)) {
+        this[key] = opts[key];
+      }
+    });
+  }
+
+  convert() {
+    var convertFile = new ConvertFilePeriodo({
+      origin_file_path: this.paste_data_path
+    });
+    convertFile._readItauCopyPasteFileAsync()
+    .then(convertFile.removeAllCrap.bind(convertFile))
+    .then(function() {
+      var result = convertFile.convertEachItauLine();
+      console.log(result);
+    });
+  }
+};
